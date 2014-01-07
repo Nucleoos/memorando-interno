cf4382e7d9202cd95d4fe2fbfdb3499439919e3b
luizfernandoafrabrito <luizfernandoafrabrito@hotmail.com>
2014-01-07 18:37:24 +0000

    Alterei vCriar e cSalvar.

--- cSalva-mi(copia).php
+++ cSalva-mi(copia).php
@@ -15,31 +15,34 @@ session_start("usuario");
     $txtTitulo = $_POST['titulo'];
     $txtCorpo = $_POST['corpo'];
     $dataEmissao = $_POST['data'];
-
+    
+    $numeroMemorando = $_SESSION["numeroMemorando"];
+    
+    echo $numeroMemorando;
     $usuario = $_SESSION["login"];
-
+    
     $bdMi = new MYSQL_MIDB();
     
     $numeroMemorando = $_SESSION["numeroMemorando"];
+    
+    
     
     $selectMemorando = $bdMi->sql("SELECT * FROM memorando WHERE idMemorando=$numeroMemorando");
     
     
     $resultado = $bdMi->sql("SELECT idUsuario FROM usuario WHERE emailInstitucional = '$usuario'");
     $idUsuarioAtual = mysql_result($resultado, 0, 'idUsuario');
-  
+    
     if(mysql_num_rows($selectMemorando) == 0 ){
         $insertMemorando = $bdMi->sql( "INSERT INTO memorando(data, remetente, destinatario, cargo, titulo, corpo ) 
                                        VALUES( '$dataEmissao', '$idUsuarioAtual', '1', '$txtCargoRemetente', '$txtTitulo', '$txtCorpo' )");
         //Verificação de sucesso na inserção da unidade
         if ($insertMemorando > 0) {
-            
-            return 1;
-            
+          
+          echo "true";
         } else {
             
-            return 3;
-            
+          echo "false";  
         }
         
     } else{
@@ -50,11 +53,11 @@ session_start("usuario");
         //Verificação de sucesso na atualizacao da unidade
         if ($updateMemorando > 0) {
             
-            return 1;
+            echo "true";
 
         } else {
             
-            return 3;
+            echo "false";
             
         }
     }
