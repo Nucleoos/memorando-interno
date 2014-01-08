a22f0f1f50c58e7c5a5b3aa16cd178318187dfea
luizfernandoafrabrito <luizfernandoafrabrito@hotmail.com>
2014-01-08 14:33:19 +0000

    Botao Salvar concluído Agora consegue-se salvar o MI durante sua edição

--- cSalva-mi(copia).php
+++ cSalva-mi(copia).php
@@ -7,7 +7,6 @@ session_start("usuario");
     //Inclusão do arquivo de conexão com o banco e instanciamento da conexao
     include("conecta-mi-db.php");    
     
-    
     //**********RECEBIMENTO DAS VARIÁRVEIS**********/
     $txtDestinatario = $_POST['destinatario'];
     $txtCargoRemetente = $_POST['cargo'];
@@ -15,49 +14,43 @@ session_start("usuario");
     $txtTitulo = $_POST['titulo'];
     $txtCorpo = $_POST['corpo'];
     $dataEmissao = $_POST['data'];
+    $numeroMemorando = $_POST['numeroMemorando'];
     
-    $numeroMemorando = $_SESSION["numeroMemorando"];
-    
-    echo $numeroMemorando;
     $usuario = $_SESSION["login"];
-    
+   
     $bdMi = new MYSQL_MIDB();
-    
-    $numeroMemorando = $_SESSION["numeroMemorando"];
-    
-    
-    
-    $selectMemorando = $bdMi->sql("SELECT * FROM memorando WHERE idMemorando=$numeroMemorando");
-    
     
     $resultado = $bdMi->sql("SELECT idUsuario FROM usuario WHERE emailInstitucional = '$usuario'");
     $idUsuarioAtual = mysql_result($resultado, 0, 'idUsuario');
     
+    $selectMemorando = $bdMi->sql("SELECT * FROM memorando WHERE idMemorando=$numeroMemorando");
+     
     if(mysql_num_rows($selectMemorando) == 0 ){
         $insertMemorando = $bdMi->sql( "INSERT INTO memorando(data, remetente, destinatario, cargo, titulo, corpo ) 
                                        VALUES( '$dataEmissao', '$idUsuarioAtual', '1', '$txtCargoRemetente', '$txtTitulo', '$txtCorpo' )");
         //Verificação de sucesso na inserção da unidade
         if ($insertMemorando > 0) {
           
-          echo "true";
+          echo true;
         } else {
             
-          echo "false";  
+          echo false;  
         }
         
     } else{
+        
         $updateMemorando = $bdMi->sql("UPDATE memorando 
-                                        SET data = $dataEmissao, remetente = $idUsuarioAtual, destinatario = 1, cargo = $txtCargoRemetente, titulo = $txtTitulo, corpo = $txtCorpo
+                                        SET data = '$dataEmissao', remetente = $idUsuarioAtual, destinatario = 1, cargo = '$txtCargoRemetente', titulo = '$txtTitulo', corpo = '$txtCorpo'
                                         WHERE idMemorando = $numeroMemorando");
         
         //Verificação de sucesso na atualizacao da unidade
         if ($updateMemorando > 0) {
             
-            echo "true";
+            echo true;
 
         } else {
             
-            echo "false";
+            echo false;
             
         }
     }
