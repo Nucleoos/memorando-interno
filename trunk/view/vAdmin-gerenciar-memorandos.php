08bd8da4369133f3001a3f0aeaa8725e18d1715c
luizfernandoafrabrito@hotmail.com <luizfernandoafrabrito@hotmail.com@f17f1ae9-8147-8433-d4a6-8fc544b97e35>
2014-02-07 00:30:44 +0000

    cadastro funcionando

--- trunk/view/vAdmin-gerenciar-memorandos.php
+++ trunk/view/vAdmin-gerenciar-memorandos.php
@@ -42,9 +42,31 @@ if (isset($_SESSION["login"]) and ($_SESSION["senha"])) {
         <script type="text/javascript">
 
                     $(document).ready(function () {
-
+                      
+                        var ptbrMessages = {
+                                serverCommunicationError: 'Erro ao tentar conexão com o servidor.',
+                                loadingMessage: 'Carregando registros...',
+                                noDataAvailable: 'Não existem dados a serem exibibos no momento!',
+                                addNewRecord: '+ Adicionar novo registro',
+                                editRecord: 'Editar registro',
+                                areYouSure: 'Você tem certeza ?',
+                                deleteConfirmation: 'Este registro será excluido. Confirmar ?',
+                                save: 'Salvar',
+                                saving: 'Salvando...',
+                                cancel: 'Cancelar',
+                                deleteText: 'Excluir',
+                                deleting: 'Excluindo...',
+                                error: 'Erro',
+                                close: 'Fechar',
+                                cannotLoadOptionsFor: 'Não foi possivel carregar opções para o campo {0}!',
+                                pagingInfo: 'Exibindo registros {0} a {1} de {2}',
+                                canNotDeletedRecords: 'Não foi possível excluir registro(s) {0} de {1}!',
+                                deleteProggress: 'Excluindo {0} de {1} registros, processando...'
+                        };
+                        
                         //Prepare jTable
                             $('#MiTableContainer').jtable({
+                                    messages: ptbrMessages,
                                     title: 'Memorandos',
                                     paging: true,
                                     pageSize: 10,
