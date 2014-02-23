e1c8e70fca1113cdae6892b343c22d1114c06333
luizfernandoafrabrito@hotmail.com <luizfernandoafrabrito@hotmail.com@f17f1ae9-8147-8433-d4a6-8fc544b97e35>
2014-02-23 22:59:05 +0000

    fazendo upload

--- trunk/control/cSalva-mi.php
+++ trunk/control/cSalva-mi.php
@@ -18,63 +18,74 @@ session_start("usuario");
     
     $bdMi = new MYSQL_MIDB();
     
-    $resultado = $bdMi->sql("SELECT idDestinatario FROM destinatario WHERE nome = '$nomeDestinatario' AND titulo = '$tituloDestinatario' and cargo = '$cargoDestinatario'");
-    
-    if(!mysql_num_rows($resultado) )
-    {
-        $resultado = $bdMi->sql("INSERT INTO destinatario(nome, titulo, cargo) VALUES ( '$nomeDestinatario', '$tituloDestinatario', '$cargoDestinatario' )");
-    }
- 
-    
-    $referencia = $_POST['referencia'];
-    
-    $corpo = $_POST['corpo'];
-    
-    $dataEmissao = $_POST['data'];
-    
     $numeroMemorando = $_POST['numeroMemorando'];
     
-    $emissario = $_POST['emissario'];
+    $resultado = $bdMi->sql( "SELECT nMemorandoEmitido FROM memorando WHERE idMemorando = $numeroMemorando" );
     
-    $usuario = $_SESSION["login"];
-  
+    if( mysql_result($resultado, 0, 'nMemorandoEmitido') == NULL )
+    {
     
-    $resultado = $bdMi->sql("SELECT idUsuario FROM usuario WHERE emailInstitucional = '$usuario'");
-    $idRemetente = mysql_result($resultado, 0, 'idUsuario');
-    
-    $resultado = $bdMi->sql("SELECT idDestinatario FROM destinatario WHERE nome = '$nomeDestinatario' AND titulo = '$tituloDestinatario' and cargo = '$cargoDestinatario'");
-    $idDestinatario = mysql_result($resultado, 0, 'idDestinatario');
-    
-    $selectMemorando = $bdMi->sql("SELECT * FROM memorando WHERE idMemorando=$numeroMemorando");
-     
-    if(mysql_num_rows($selectMemorando) == 0 ){
-        $insertMemorando = $bdMi->sql( "INSERT INTO memorando(data, remetente, destinatario, referencia, corpo, emissario ) 
-                                       VALUES( '$dataEmissao', '$idRemetente', '$idDestinatario', '$referencia', '$corpo', '$emissario' )");
-        //Verificação de sucesso na inserção da unidade
-        if ($insertMemorando > 0) {
-          
-          echo true;
-        } else {
-            
-          echo false;  
-        }
-        
-    } else{
-        
-        $updateMemorando = $bdMi->sql("UPDATE memorando 
-                                        SET data = '$dataEmissao', remetente = $idRemetente, destinatario = $idDestinatario, referencia = '$referencia', corpo = '$corpo', emissario = $emissario
-                                        WHERE idMemorando = $numeroMemorando");
-        
-        //Verificação de sucesso na atualizacao da unidade
-        if ($updateMemorando > 0) {
-            
-            echo true;
+        $resultado = $bdMi->sql("SELECT idDestinatario FROM destinatario WHERE nome = '$nomeDestinatario' AND titulo = '$tituloDestinatario' and cargo = '$cargoDestinatario'");
 
-        } else {
-            
-            echo false;
-            
+        if(!mysql_num_rows($resultado) )
+        {
+            $resultado = $bdMi->sql("INSERT INTO destinatario(nome, titulo, cargo) VALUES ( '$nomeDestinatario', '$tituloDestinatario', '$cargoDestinatario' )");
         }
+
+
+        $referencia = $_POST['referencia'];
+
+        $corpo = $_POST['corpo'];
+
+        $dataEmissao = $_POST['data'];
+
+
+        $emissario = $_POST['emissario'];
+
+        $usuario = $_SESSION["login"];
+
+
+        $resultado = $bdMi->sql("SELECT idUsuario FROM usuario WHERE emailInstitucional = '$usuario'");
+        $idRemetente = mysql_result($resultado, 0, 'idUsuario');
+
+        $resultado = $bdMi->sql("SELECT idDestinatario FROM destinatario WHERE nome = '$nomeDestinatario' AND titulo = '$tituloDestinatario' and cargo = '$cargoDestinatario'");
+        $idDestinatario = mysql_result($resultado, 0, 'idDestinatario');
+
+        $selectMemorando = $bdMi->sql("SELECT * FROM memorando WHERE idMemorando=$numeroMemorando");
+
+        if(mysql_num_rows($selectMemorando) == 0 ){
+            $insertMemorando = $bdMi->sql( "INSERT INTO memorando(data, remetente, destinatario, referencia, corpo, emissario ) 
+                                           VALUES( '$dataEmissao', '$idRemetente', '$idDestinatario', '$referencia', '$corpo', '$emissario' )");
+            //Verificação de sucesso na inserção da unidade
+            if ($insertMemorando > 0) {
+
+              echo true;
+            } else {
+
+              echo false;  
+            }
+
+        } else{
+
+            $updateMemorando = $bdMi->sql("UPDATE memorando 
+                                            SET data = '$dataEmissao', remetente = $idRemetente, destinatario = $idDestinatario, referencia = '$referencia', corpo = '$corpo', emissario = $emissario
+                                            WHERE idMemorando = $numeroMemorando");
+
+            //Verificação de sucesso na atualizacao da unidade
+            if ($updateMemorando > 0) {
+
+                echo true;
+
+            } else {
+
+                echo false;
+
+            }
+        }
+    }
+    else
+    {
+        echo false;
     }
     
 ?>
